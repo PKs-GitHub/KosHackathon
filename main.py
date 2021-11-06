752c5b325deac440a6d0df09662a8769ad5bfdb4
yoonseulgi <tmffml1232@gmail.com>
2021-11-06 17:18:34 +0900

    1. Date: 21.11.06 2. Jira: 3. Description - Test for commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from pykiwoom.kiwoom import *
+
+kiwoom = Kiwoom()
+print("블록킹 로그인 완료")
+kiwoom.CommConnect(block=True)
+print("블록킹 로그인 완료")
+
+#import sys
+
+#for p in sys.path:
+#    print(p)
+
+    
